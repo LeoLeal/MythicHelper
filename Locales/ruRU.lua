7bfce8da5860c4f684a5d99a391907a4c9a70e68
Leonardo Leal <dev@leoleal.com.br>
2017-02-13 21:02:36 -0200

    feat(localization): Added ru-RU language. (Thanks to darhanger at curse.com)

---
+++ Locales/ruRU.lua
@@ -0,0 +1,14 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("MythicHelper", "ruRU")
+if L == nil then
+    return
+end
+
+L["Chests"] = "Сундук"
+L["NoChests"] = "1 Сундук"
+L["NoChests_KeyDepleted"] = "1 Сундук - израсходован"
+L["Rewards"] = "Награды"
+L["No_Loot"] = "Без добычи"
+L["Loot"] = "Добыча"
+L["BaseLootLevel"] = "Вознаграждение как минимум %d уровня предмета если не израсходован."
+L["WeeklyChestText"] = "На следующей неделе, Вы получит предмет как минимум |cff00ff00%d |rуровня с Вашего Роскошного дара претендента в оплоте класса. "
+L["EmptyWeeklyChestText"] = "Пройдите несколько подземелий с эпохальным ключом, что бы получить награду на следующей неделе"
