13d116cdcb70c44c78ac58766f9b27b3f3f87621
Leonardo Leal <dev@leoleal.com.br>
2017-03-30 22:23:41 -0300

    fix(tooltip): tooltip on other items.

--- KeystoneTooltip.lua
+++ KeystoneTooltip.lua
@@ -2,6 +2,15 @@ local function GetModifiers(linkType, ...)
 	if type(linkType) ~= 'string' then return end
 	local modifierOffset = 4
 	local instanceID, mythicLevel, notDepleted, _ = ... -- "keystone" links
+	if mythicLevel and mythicLevel ~= "" then
+		mythicLevel = tonumber(mythicLevel);
+		if mythicLevel and mythicLevel > 15 then
+			mythicLevel = 15;
+		end
+	else
+		mythicLevel = nil;
+	end
+
 	if linkType:find('item') then -- only used for ItemRefTooltip currently
 		_, _, _, _, _, _, _, _, _, _, _, _, _, instanceID, mythicLevel = ...
 		if ... == '138019' then -- mythic keystone
@@ -25,22 +34,17 @@ local function GetModifiers(linkType, ...)
 end
 
 local function DecorateTooltip(self)
-	local _, link = self:GetItem()
-	if type(link) == 'string' then
+	local _, link = self:GetItem();
+	if type(link) == 'string' and link:find("keystone") then
 		local modifiers, instanceID, mythicLevel = GetModifiers(strsplit(':', link))
 		if modifiers then
-			mythicLevel = tonumber(mythicLevel);
-
-			if mythicLevel and mythicLevel > 15 then
-				mythicLevel = 15;
-			end
 			for _, modifierID in ipairs(modifiers) do
 				local modifierName, modifierDescription = C_ChallengeMode.GetAffixInfo(modifierID)
 				if modifierName and modifierDescription then
 					self:AddLine(format('\n|cff00ff00%s: |cffffffff%s|r', modifierName, modifierDescription), 0, 1, 0, true)
 				end
 			end
-			if mythicLevel and mythicLevel > 0 then
+			if type(mythicLevel) == "number" and mythicLevel > 0 then
 				self:AddLine(format('\n|cffffcc00%s|r', format(MythicHelper.L["BaseLootLevel"], MYTHIC_CHEST_TIMERS_LOOT_ILVL[mythicLevel])), 0, 1, 0, true)
 			end
 			self:Show()
