e7d88b7949bb088a5c02e2fa865b0d6057a2b844
Leonardo Leal <dev@leoleal.com.br>
2017-06-15 01:01:26 -0300

    feat(timers): changed to the new key upgrade and loot system in 7.2.5

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,4 +1,4 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("MythicHelper", "frFR", true)
+local L = LibStub("AceLocale-3.0"):NewLocale("MythicHelper", "enUS", true)
 
 if L == nil then
   return
@@ -6,12 +6,8 @@ end
 
 L["FONT"] = "Fonts\\FRIZQT__.TTF"
 
-L["Chests"] = "Chests"
-L["NoChests"] = "1 Chest"
-L["NoChests_KeyDepleted"] = "1 Chest - depleted"
-L["Rewards"] = "Rewards"
-L["No_Loot"] = "No loot"
-L["Loot"] = "Loot"
-L["BaseLootLevel"] = "Reward of at least %d item level if not depleted."
+L["BaseLootLevel"] = "Reward of at least %d item level."
 L["WeeklyChestText"] = "Next week you will receive an item of at least |cff00ff00%d |rlevel from your Challenger's Bounty Chest in your Order Hall."
 L["EmptyWeeklyChestText"] = "Run some Mythic+ Dungeons to get some bounty next week."
+
+L["Keystone_Level"] = "Key Levels"
--- Locales/ptBR.lua
+++ Locales/ptBR.lua
@@ -1,15 +1,13 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("MythicHelper", "ptBR")
+
 if L == nil then
     return
 end
+
 L["FONT"] = "Fonts\\FRIZQT__.TTF"
 
-L["Chests"] = "Baús"
-L["NoChests"] = "1 Baú"
-L["NoChests_KeyDepleted"] = "1 Baú - Esgotado"
-L["Rewards"] = "Recompensas"
-L["No_Loot"] = "Sem saque"
-L["Loot"] = "Saque"
 L["BaseLootLevel"] = "Recompensa com no mínimo %d de nível de item quando não esgotada."
 L["WeeklyChestText"] = "Na próxima semana você receberá um item de ao menos |cff00ff00%d |rde nível do Baú do Desafiante em seu Salão da Ordem de Classe."
 L["EmptyWeeklyChestText"] = "Faça uma Masmorra Mítica+ para garantir sua recompensa na próxima semana."
+
+L["Keystone_Level"] = "Níveis da Chave"
