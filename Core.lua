5416929c2b57f537b55da52d51db9d406245ac4c
Leonardo Leal <dev@leoleal.com.br>
2018-09-11 23:50:26 +0200

    feat(slash): added slash command for quick prize reference. Type '/mythic' to see Keystone Levels Dungeon Rewards.

--- Core.lua
+++ Core.lua
@@ -1,6 +1,36 @@
 MythicHelper = LibStub("AceAddon-3.0"):NewAddon("MythicHelper", "AceEvent-3.0", "AceTimer-3.0");
 MYTHIC_CHEST_TIMERS_LOOT_ILVL = {340,345,345,350,355,355,360,365,365,370,370,370,370,370,370};
 MYTHIC_CHEST_TIMERS_AZERITE_ILVL = {340,340,340,355,355,355,370,370,370,385,385,385,385,385,385};
+SLASH_MYTHIC1 = '/mythic'
+SlashCmdList['MYTHIC'] = function ()
+  print("|cFFFFFFFFMythic Dungeon Chest rewards");
+  print("|cFFFFFFFF=====================================");
+  for i = 1, 15 do
+    levelText = "";
+
+    if i == 1 then
+      levelText = "Base Mythic Level";
+    else
+      levelText = "Keystone Level " .. i;
+    end
+    if i == 15 then
+      levelText = levelText .. "+";
+    end
+
+    levelColor = "|cFFFFFFFF";
+    if i > 4 then
+      levelColor = "|cFF00FF00";
+    end
+    if i > 9 then
+      levelColor = "|cFF3377FF";
+    end
+    if i > 12 then
+      levelColor = "|cFFCC00FF";
+    end
+
+    print("|cFFFFFF00" .. levelText .. ": ".. levelColor .. MYTHIC_CHEST_TIMERS_LOOT_ILVL[i]);
+  end
+end
 -- ---------------------------------------------------------------------------------------------------------------------
 function MythicHelper:OnInitialize()
   MythicHelper.L = LibStub("AceLocale-3.0"):GetLocale("MythicHelper")
