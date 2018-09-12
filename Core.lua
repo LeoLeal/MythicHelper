5110c62b259a864a19cf654bcfb14e53cafcdbe4
Leonardo Leal <dev@leoleal.com.br>
2018-09-12 20:08:22 +0200

    feat(slash-command): enhanced slash command. Now you can do '/mythic' to see all the rewards table for Dungeon and Weekly Chest or '/mythic <level number>' to see the reward for that particular keystone level.

--- Core.lua
+++ Core.lua
@@ -1,34 +1,28 @@
 MythicHelper = LibStub("AceAddon-3.0"):NewAddon("MythicHelper", "AceEvent-3.0", "AceTimer-3.0");
 MYTHIC_CHEST_TIMERS_LOOT_ILVL = {340,345,345,350,355,355,360,365,365,370,370,370,370,370,370};
 MYTHIC_CHEST_TIMERS_AZERITE_ILVL = {340,340,340,355,355,355,370,370,370,385,385,385,385,385,385};
+MYTHIC_CHEST_TIMERS_WEEKLY_ILVL = {0,355,355,360,360,365,370,370,375,380,380,380,380,380,380};
+
+MYTHIC_KEYSTONE_TEXT_TABLE = {};
 SLASH_MYTHIC1 = '/mythic'
-SlashCmdList['MYTHIC'] = function ()
-  print("|cFFFFFFFFMythic Dungeon Chest rewards");
-  print("|cFFFFFFFF=====================================");
-  for i = 1, 15 do
-    levelText = "";
 
-    if i == 1 then
-      levelText = "Base Mythic Level";
+function SlashCmdList.MYTHIC(msg, editbox)
+  print("|cFFFFFFFF======================================================");
+  print("|cFFFFFFFF= Mythic Dungeon Chest rewards");
+  print("|cFFFFFFFF======================================================");
+
+  if tonumber(msg) == nil then
+    for i = 1, 15 do
+      print(MYTHIC_KEYSTONE_TEXT_TABLE[i]);
+    end
+  else
+    if MYTHIC_KEYSTONE_TEXT_TABLE[tonumber(msg)] == nil then
+      for i = 1, 15 do
+        print(MYTHIC_KEYSTONE_TEXT_TABLE[i]);
+      end
     else
-      levelText = "Keystone Level " .. i;
+      print(MYTHIC_KEYSTONE_TEXT_TABLE[tonumber(msg)]);
     end
-    if i == 15 then
-      levelText = levelText .. "+";
-    end
-
-    levelColor = "|cFFFFFFFF";
-    if i > 4 then
-      levelColor = "|cFF00FF00";
-    end
-    if i > 9 then
-      levelColor = "|cFF3377FF";
-    end
-    if i > 12 then
-      levelColor = "|cFFCC00FF";
-    end
-
-    print("|cFFFFFF00" .. levelText .. ": ".. levelColor .. MYTHIC_CHEST_TIMERS_LOOT_ILVL[i]);
   end
 end
 -- ---------------------------------------------------------------------------------------------------------------------
@@ -37,6 +31,41 @@ function MythicHelper:OnInitialize()
 
   MythicHelperCMTimer:Init();
   MythicHelperKeystoneTooltip:Init();
+  FeedKeystoneTextTable();
+end
+
+function FeedKeystoneTextTable()
+  for i = 1, 15 do
+    levelPrefix = "Keystone Level ";
+    weeklyChestText = " | Weekly Chest - " .. MYTHIC_CHEST_TIMERS_LOOT_ILVL[i] .. "+";
+
+    if i == 1 then
+      levelText = "";
+      levelPrefix = "Base Mythic Level";
+      weeklyChestText = "";
+    else
+      levelText = i;
+      if i < 10 then
+        levelText = "0"..i;
+      end
+    end
+
+    levelColor = "|cFFFFFFFF";
+    if i == 1 then
+      levelColor = "|cFF999999";
+    end
+    if i > 3 then
+      levelColor = "|cFF00FF00";
+    end
+    if i > 6 then
+      levelColor = "|cFF3377FF";
+    end
+    if i > 9 then
+      levelColor = "|cFFCC00FF";
+    end
+
+    MYTHIC_KEYSTONE_TEXT_TABLE[i] = "|cFFFFFF00[ " .. levelPrefix .. levelText .. " ] " .. levelColor .. "Dungeon Chest - " .. MYTHIC_CHEST_TIMERS_LOOT_ILVL[i] .. "+" .. weeklyChestText;
+  end
 end
 
 -- ---------------------------------------------------------------------------------------------------------------------
